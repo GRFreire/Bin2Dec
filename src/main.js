53164baffd9eac8efc72b2490c7c981443f190dd
GRFreire <gui.rugai.freire@gmail.com>
2020-02-23 19:00:17 -0300

    feat: add binary to decimal convertion

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-unused-vars
+function convert() {
+    const binary = document.getElementById('b-input').value;
+    const decimal = parseInt(binary, 2);
+    document.getElementById('d-input').value = decimal;
+}
