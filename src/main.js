1308ef9826cba46c328ddc493465669e76cc2b93
GRFreire <gui.rugai.freire@gmail.com>
2020-02-23 19:36:48 -0300

    feat: add verification and alert for no binary entries

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
+/* eslint-disable consistent-return */
+/* eslint-disable array-callback-return */
+/* eslint-disable no-alert */
 // eslint-disable-next-line no-unused-vars
 function convert() {
     const binary = document.getElementById('b-input').value;
+    binary.split('').map((char) => {
+        if (char !== '0' && char !== '1') return alert('Please, enter a binary number');
+    });
     const decimal = parseInt(binary, 2);
     document.getElementById('d-input').value = decimal;
+    return true;
 }
