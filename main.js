7d608350d5c085747266754261973dbc77b20635
GRFreire <gui.rugai.freire@gmail.com>
2020-02-23 19:42:38 -0300

    fix: add empty entry to verify binary numbers function

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 // eslint-disable-next-line no-unused-vars
 function convert() {
     const binary = document.getElementById('b-input').value;
+    if (binary === '') return alert('Please, enter a binary number');
     binary.split('').map((char) => {
         if (char !== '0' && char !== '1') return alert('Please, enter a binary number');
     });
